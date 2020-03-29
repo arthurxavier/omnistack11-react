9635cea9856cb25c826598a833c8d196f5b75457
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 17:45:10 -0300

    Implementing  logon page HTML

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -10,6 +10,15 @@ function Logon() {
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" />
+          <button type="submit">Entrar</button>
+
+          <a href="/register">Não tenho cadastro</a>
+        </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
     </div>
