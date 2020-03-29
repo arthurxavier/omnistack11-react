fbc1203cd52b3789eb1eaff0a76b1a63c54087c0
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 17:51:58 -0300

    Adding FiLogIn  by imports react-icons/fi

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiLogIn } from "react-icons/fi";
 
 import "./styles.css";
 
@@ -17,7 +18,10 @@ function Logon() {
           <input placeholder="Sua ID" />
           <button type="submit">Entrar</button>
 
-          <a href="/register">Não tenho cadastro</a>
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </a>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
