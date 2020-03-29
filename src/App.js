c87543e91002bbc24103f87da7a879cb73a5983e
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 16:32:02 -0300

    Adding increment component stateless

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import Header from "./Header";
 
 function App() {
-  return <Header>Semana Omnistack</Header>;
+  let counter = 0;
+
+  function incrementor() {
+    counter++;
+    console.log(counter);
+  }
+
+  return (
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={incrementor}>Incrementar</button>
+    </div>
+  );
 }
 
 export default App;
