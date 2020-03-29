5db8890a10c22b22b7a275d59ffe964b14d170e8
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 19:23:26 -0300

    Fix route back-lin in Register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -19,7 +19,7 @@ function Register() {
             os casos da sua ONG.
           </p>
 
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#E02041" />
             Não tenho cadastro
           </Link>
