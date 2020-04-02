0d559e0fbbe655bed3d1d8265248635b3ea55c31
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-04-01 23:32:48 -0300

    Adding feature delete incidents  by api. Adding re setIncidents after delete one.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -25,6 +25,20 @@ function Profile() {
       });
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      setIncidents(incidents.filter(incidents => incidents.id !== id));
+    } catch (err) {
+      alert("Erro ao deletar caso, tente novamente.");
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -58,7 +72,10 @@ function Profile() {
               }).format(incident.value)}
             </p>
 
-            <button type="button">
+            <button
+              onClick={() => handleDeleteIncident(incidents.id)}
+              type="button"
+            >
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
