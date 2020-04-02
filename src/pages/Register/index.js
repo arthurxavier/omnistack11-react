bdef6cf9e8d372266c7280679a9d9f196936f3a3
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-04-01 22:13:58 -0300

    Adding push to route '/' after sucess request

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
 import api from "../../services/api";
@@ -9,11 +9,13 @@ import "./styles.css";
 import logoImg from "../../assets/logo.svg";
 
 function Register() {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [whatsapp, setWhatsapp] = useState();
-  const [city, setCity] = useState();
-  const [uf, setUf] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  const history = useHistory();
 
   async function handleRegister(e) {
     e.preventDefault();
@@ -30,6 +32,8 @@ function Register() {
       const response = await api.post("ongs", data);
 
       alert(`Seu ID de acesso: ${response.data.id}`);
+
+      history.push("/");
     } catch (err) {
       alert("Erro no cadastro, tente novamente.");
     }
